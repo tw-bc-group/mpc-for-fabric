00aaad753950b6c334ae8eb91d85054f6ea4af7f
Matrix <113445886@qq.com>
2020-07-28 17:06:33 +0800

    feat: add third party to chaincode

--- demo/demo.go
+++ demo/demo.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/testdata"
 	"log"
 	"time"
+
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/testdata"
 
 	"github.com/LabZion/HEaaS/common"
 	pb "github.com/LabZion/HEaaS/fhe"
@@ -20,7 +21,7 @@ import (
 var (
 	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
 	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
-	serverAddr         = flag.String("server_addr", "172.24.82.229:10000", "The server address in the format of host:port")
+	serverAddr         = flag.String("server_addr", "172.31.81.128:10000", "The server address in the format of host:port")
 	serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "The server name used to verify the hostname returned by the TLS handshake")
 )
 
@@ -52,14 +53,15 @@ func generateKeysRemote(client pb.FHEClient) KeyPair {
 	}
 }
 
-// storePublicKey store a pair of fhe keys
-func storePublicKey(client pb.FHEClient, account string, keyPair KeyPair) {
+// storeKey store a pair of fhe keys
+func storeKey(client pb.FHEClient, account string, keyPair KeyPair) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	_, err := client.StorePublicKey(ctx, &pb.StoreKeyRequest{
+	_, err := client.StoreKey(ctx, &pb.StoreKeyRequest{
 		Account: account,
 		KeyPair: &pb.KeyPair{
 			PublicKey: keyPair.PublicKey,
+			SecretKey: keyPair.SecretKey,
 		},
 	})
 	if err != nil {
@@ -86,6 +88,31 @@ func setAsk(client pb.FHEClient, keyPair KeyPair, account string, limit int) {
 	})
 	if err != nil {
 		log.Fatalf("%v.SetAsk(_) = _, %v: ", client, err)
+	}
+	return
+}
+
+// setBid set an bid for account
+func setBid(client pb.FHEClient, keyPair KeyPair, targetAccount string, account string, limit int, credit int) {
+	params := common.GetParams()
+
+	pk := bfv.PublicKey{}
+	pk.UnmarshalBinary(keyPair.PublicKey)
+	encryptorPk := bfv.NewEncryptorFromPk(params, &pk)
+
+	limitCiphertextBytes := common.EncryptInt(encryptorPk, limit)
+	creditCiphertextBytes := common.EncryptInt(encryptorPk, credit)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	_, err := client.SetBid(ctx, &pb.BidRequest{
+		TargetAccount:        targetAccount,
+		Account:              account,
+		LimitPriceCipherText: limitCiphertextBytes,
+		CreditCipherText:     creditCiphertextBytes,
+	})
+	if err != nil {
+		log.Fatalf("%v.SetBid(_) = _, %v: ", client, err)
 	}
 	return
 }
@@ -120,10 +147,23 @@ func getEligibleBids(client pb.FHEClient, keyPair KeyPair, account string) (uint
 	return eligibleBidResponse.TotalBidNumber, bids
 }
 
+func fetchPublicKey(client pb.FHEClient, account string) KeyPair {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	keyPair, err := client.FetchPublicKey(ctx, &pb.FetchPublicKeyRequest{
+		Account: account,
+	})
+	if err != nil {
+		log.Fatalf("%v.FetchPublicKey(_) = _, %v: ", client, err)
+	}
+	return KeyPair{
+		PublicKey: keyPair.PublicKey,
+		SecretKey: keyPair.SecretKey,
+	}
+}
+
 type SmartContract struct {
 	contractapi.Contract
-	client pb.FHEClient
-	kp KeyPair
 }
 
 func newClient() pb.FHEClient {
@@ -154,24 +194,38 @@ func newClient() pb.FHEClient {
 	return client
 }
 
-
 func (s *SmartContract) SetAsk(_ contractapi.TransactionContextInterface) (string, error) {
+	client := newClient()
+	kp := generateKeysRemote(client)
+	storeKey(client, account, kp)
 	limit := 100
 	fmt.Println("Saving Ask.")
-	setAsk(s.client, s.kp, account, limit)
-	return "success", nil
+	setAsk(client, kp, account, limit)
+	return "SetAsk succeeded", nil
 }
 
 func (s *SmartContract) GetBids(_ contractapi.TransactionContextInterface) (string, error) {
-	number, bids := getEligibleBids(s.client, s.kp, account)
+	client := newClient()
+	kp := fetchPublicKey(client, account)
+	number, bids := getEligibleBids(client, kp, account)
 	return fmt.Sprintf("total bid number: %d, bids: %#v\n", number, bids), nil
 }
 
-func main() {
+func (s *SmartContract) SetBids(_ contractapi.TransactionContextInterface) (string, error) {
 	client := newClient()
-	smartContract := SmartContract {client: client, kp: generateKeysRemote(client)}
-	storePublicKey(client, account, smartContract.kp)
-	chainCode, err := contractapi.NewChaincode(&smartContract)
+	kp := fetchPublicKey(client, account)
+
+	limit := 100
+	credit := 630
+	setBid(client, kp, account, "alice@gmail.com", limit+10, credit+100)
+	setBid(client, kp, account, "bob@gmail.com", limit, credit-100)
+	setBid(client, kp, account, "evan@gmail.com", limit-10, credit)
+
+	return "SetBids succeeded", nil
+}
+
+func main() {
+	chainCode, err := contractapi.NewChaincode(new(SmartContract))
 
 	if err != nil {
 		fmt.Printf("Error create demo chainCode: %s", err.Error())
